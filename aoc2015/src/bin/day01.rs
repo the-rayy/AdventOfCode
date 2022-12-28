750f7394a2f1250059d1bb78691cd44fb973b1eb
Rafał Klusek <klusek.raf@gmail.com>
2022-12-28 23:56:58 +0000

    day01 part2

--- aoc2015/src/bin/day01.rs
+++ aoc2015/src/bin/day01.rs
@@ -10,18 +10,33 @@ fn main() {
     println!("Part 1 time: {:.2?}", part1_start.elapsed());
     println!("Part 1 ans: {:?}", part1_ans);
 
-//    let part2_start = Instant::now();
-//    let part2_ans = part2(&input);
-//    println!("Part 2 time: {:.2?}", part2_start.elapsed());
-//    println!("Part 2 ans: {:?}", part2_ans);
+    let part2_start = Instant::now();
+    let part2_ans = part2(&input);
+    println!("Part 2 time: {:.2?}", part2_start.elapsed());
+    println!("Part 2 ans: {:?}", part2_ans);
 }
 
 fn part1(input: &str) -> i64 {
     input.chars()
-    .map(|c| match c {
-        '(' => 1,
-        ')' => -1,
-        _ => unreachable!()
-    })
-    .sum()
+        .map(|c| match c {
+            '(' => 1,
+            ')' => -1,
+            _ => unreachable!()
+        })
+        .sum()
+}
+
+fn part2(input: &str) -> usize {
+    input.chars()
+        .map(|c| match c {
+            '(' => 1,
+            ')' => -1,
+            _ => unreachable!()
+        })
+        .scan(0, |acc, v| {
+            *acc = *acc + v;
+            Some(*acc)
+        })
+        .position(|v| v == -1)
+        .unwrap() + 1
 }
