ccb25ad4d18d1d374b496d670bae1cefce16b88b
Rafał Klusek <klusek.raf@gmail.com>
2022-12-29 01:40:25 +0100

    day03 part1

---
+++ aoc2015/src/bin/day03.rs
@@ -0,0 +1,35 @@
+use std::fs;
+use std::time::Instant;
+use std::collections::HashSet;
+
+fn main() {
+    let input = fs::read_to_string("data/day03.txt")
+        .expect("Unable to load input file");
+
+    let part1_start = Instant::now();
+    let part1_ans = part1(&input);
+    println!("Part 1 time: {:.2?}", part1_start.elapsed());
+    println!("Part 1 ans: {:?}", part1_ans);
+
+    // let part2_start = Instant::now();
+    // let part2_ans = part2(&input);
+    // println!("Part 2 time: {:.2?}", part2_start.elapsed());
+    // println!("Part 2 ans: {:?}", part2_ans);
+}
+
+fn part1(input: &str) -> usize {
+    input.chars()
+        .map(|c| match c {
+            '^' => (0, 1),
+            '<' => (-1, 0),
+            '>' => (1, 0),
+            'v' => (0, -1),
+            _ => unreachable!()
+        })
+        .scan((0, 0), |acc, x| {
+            *acc = (acc.0 + x.0, acc.1 + x.1);
+            Some(*acc)
+        })
+        .collect::<HashSet<(i64, i64)>>()
+        .len()
+}
