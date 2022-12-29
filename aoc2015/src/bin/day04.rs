99f319c82bc261ae028a73f67dff3e060fe46729
Rafał Klusek <klusek.raf@gmail.com>
2022-12-29 01:59:03 +0100

    day04 part2

--- aoc2015/src/bin/day04.rs
+++ aoc2015/src/bin/day04.rs
@@ -9,10 +9,10 @@ fn main() {
     println!("Part 1 time: {:.2?}", part1_start.elapsed());
     println!("Part 1 ans: {:?}", part1_ans);
 
-    // let part2_start = Instant::now();
-    // let part2_ans = part2(&input);
-    // println!("Part 2 time: {:.2?}", part2_start.elapsed());
-    // println!("Part 2 ans: {:?}", part2_ans);
+    let part2_start = Instant::now();
+    let part2_ans = part2(&input);
+    println!("Part 2 time: {:.2?}", part2_start.elapsed());
+    println!("Part 2 ans: {:?}", part2_ans);
 }
 
 fn part1(input: &str) -> usize {
@@ -20,6 +20,17 @@ fn part1(input: &str) -> usize {
     loop {
         let digest = md5::compute(format!("{}{}", input, i));
         if format!("{:x}", digest).starts_with("00000") {
+            return i
+        }
+        i += 1;
+    }
+}
+
+fn part2(input: &str) -> usize {
+    let mut i = 0;
+    loop {
+        let digest = md5::compute(format!("{}{}", input, i));
+        if format!("{:x}", digest).starts_with("000000") {
             return i
         }
         i += 1;
