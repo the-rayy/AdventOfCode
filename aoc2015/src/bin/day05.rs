dfbd711992b6ebad40bf304f65f592777ad088da
Rafał Klusek <klusek.raf@gmail.com>
2022-12-29 21:26:18 +0100

    day05 part2

--- aoc2015/src/bin/day05.rs
+++ aoc2015/src/bin/day05.rs
@@ -11,10 +11,10 @@ fn main() {
     println!("Part 1 time: {:.2?}", part1_start.elapsed());
     println!("Part 1 ans: {:?}", part1_ans);
 
-    // let part2_start = Instant::now();
-    // let part2_ans = part2(&input);
-    // println!("Part 2 time: {:.2?}", part2_start.elapsed());
-    // println!("Part 2 ans: {:?}", part2_ans);
+    let part2_start = Instant::now();
+    let part2_ans = part2(&input);
+    println!("Part 2 time: {:.2?}", part2_start.elapsed());
+    println!("Part 2 ans: {:?}", part2_ans);
 }
 
 const VOWELS: [char; 5] = ['a', 'e', 'i', 'o', 'u'];
@@ -33,6 +33,36 @@ fn part1(input: &str) -> usize {
                 .any(|f| f);
 
             vowels_count >= 3 && line_copy.len() < line.len() && !forbidden
+        })
+        .filter(|x| *x)
+        .count()
+}
+
+fn part2(input: &str) -> usize {
+    input.split("\n")
+        .map(|line| {
+            let mut pair: bool = false;
+            'outer: for i in 0..line.len()-1 {
+                let a = &line[i..i+2];
+                for j in i+2..line.len()-1 {
+                    let b = &line[j..j+2];
+                    if a == b {
+                        pair = true;
+                        break 'outer;
+                    }
+                }
+            }
+
+            let mut repeat: bool = false;
+            for i in 0..line.len()-2 {
+                if line.chars().nth(i).unwrap() == line.chars().nth(i+2).unwrap() {
+                    repeat = true;
+                    break;
+                }
+            }
+
+            pair && repeat
+
         })
         .filter(|x| *x)
         .count()
