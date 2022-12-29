cebda69c1d307aeb1d84a4b771eeb60a7466a3dd
Rafał Klusek <klusek.raf@gmail.com>
2022-12-29 01:28:52 +0100

    day02 part2

--- aoc2015/src/bin/day02.rs
+++ aoc2015/src/bin/day02.rs
@@ -10,10 +10,10 @@ fn main() {
     println!("Part 1 time: {:.2?}", part1_start.elapsed());
     println!("Part 1 ans: {:?}", part1_ans);
 
-    // let part2_start = Instant::now();
-    // let part2_ans = part2(&input);
-    // println!("Part 2 time: {:.2?}", part2_start.elapsed());
-    // println!("Part 2 ans: {:?}", part2_ans);
+    let part2_start = Instant::now();
+    let part2_ans = part2(&input);
+    println!("Part 2 time: {:.2?}", part2_start.elapsed());
+    println!("Part 2 ans: {:?}", part2_ans);
 }
 
 fn part1(input: &str) -> i64 {
@@ -25,5 +25,18 @@ fn part1(input: &str) -> i64 {
         })
         .map(|d| [d[0]*d[1], d[1]*d[2], d[0]*d[2]])
         .map(|dim| 2 * (dim[0] + dim[1] + dim[2]) + dim.iter().min().unwrap())
+        .sum()
+}
+
+fn part2(input: &str) -> i64 {
+    input.split("\n")
+        .map(|line| {
+            let mut x = line.split("x")
+                .map(|x| x.parse::<i64>().unwrap())
+                .collect::<Vec<i64>>();
+            x.sort();
+            x
+        })
+        .map(|d| d[0]+d[0]+d[1]+d[1] + d[0]*d[1]*d[2])
         .sum()
 }
