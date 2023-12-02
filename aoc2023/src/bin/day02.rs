88ff11283469cc1654960d60927b0202b5525e51
Rafał Klusek <klusek.raf@gmail.com>
2023-12-02 12:29:19 +0100

    2023 day02 part2

--- aoc2023/src/bin/day02.rs
+++ aoc2023/src/bin/day02.rs
@@ -10,17 +10,17 @@ fn main() {
     println!("Part 1 time: {:.2?}", part1_start.elapsed());
     println!("Part 1 ans: {:?}", part1_ans);
 
-    // let part2_start = Instant::now();
-    // let part2_ans = part2(&input);
-    // println!("Part 2 time: {:.2?}", part2_start.elapsed());
-    // println!("Part 2 ans: {:?}", part2_ans);
+    let part2_start = Instant::now();
+    let part2_ans = part2(&input);
+    println!("Part 2 time: {:.2?}", part2_start.elapsed());
+    println!("Part 2 ans: {:?}", part2_ans);
 }
 
 fn part1(input: &str) -> usize {
     input.split("\n").enumerate()
         .map(|(idx, line)| {
             let sets = line.split(": ").nth(1).unwrap();
-            let sets = sets.split("; ").map(parse_set).collect::<Vec<(u32, u32, u32)>>();
+            let sets = sets.split("; ").map(parse_set).collect::<Vec<(usize, usize, usize)>>();
             let idx = idx + 1;
             let is_invalid = sets.iter().filter(|set| set.0 > 12 || set.1 > 13 || set.2 > 14).count() > 0;
             (idx, is_invalid)
@@ -30,14 +30,27 @@ fn part1(input: &str) -> usize {
         .sum::<usize>()
 }
 
-fn parse_set(set: &str) -> (u32, u32, u32) {
+fn part2(input: &str) -> usize {
+    input.split("\n")
+        .map(|line| {
+            let sets = line.split(": ").nth(1).unwrap();
+            let sets = sets.split("; ").map(parse_set).collect::<Vec<(usize, usize, usize)>>();
+            let reds = sets.iter().map(|(r, _, _)| r).max().unwrap();
+            let greens = sets.iter().map(|(_, g, _)| g).max().unwrap();
+            let blues = sets.iter().map(|(_, _, b)| b).max().unwrap();
+            reds * greens * blues
+        })
+        .sum::<usize>()
+}
+
+fn parse_set(set: &str) -> (usize, usize, usize) {
     let mut r = 0;
     let mut g = 0;
     let mut b = 0;
 
     for x in set.split(", ") {
         let mut parts = x.split(" ");
-        let count = parts.next().unwrap().parse::<u32>().unwrap();
+        let count = parts.next().unwrap().parse::<usize>().unwrap();
         let colour = parts.next().unwrap();
 
         match colour {
