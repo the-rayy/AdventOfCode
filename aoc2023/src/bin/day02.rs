5a6a31469615154cbf440faaebe180d79e7f13ee
Rafał Klusek <klusek.raf@gmail.com>
2023-12-02 12:23:43 +0100

    2023 day02 part1

---
+++ aoc2023/src/bin/day02.rs
@@ -0,0 +1,52 @@
+use std::fs;
+use std::time::Instant;
+
+fn main() {
+    let input = fs::read_to_string("data/day02.txt")
+        .expect("Unable to load input file");
+
+    let part1_start = Instant::now();
+    let part1_ans = part1(&input);
+    println!("Part 1 time: {:.2?}", part1_start.elapsed());
+    println!("Part 1 ans: {:?}", part1_ans);
+
+    // let part2_start = Instant::now();
+    // let part2_ans = part2(&input);
+    // println!("Part 2 time: {:.2?}", part2_start.elapsed());
+    // println!("Part 2 ans: {:?}", part2_ans);
+}
+
+fn part1(input: &str) -> usize {
+    input.split("\n").enumerate()
+        .map(|(idx, line)| {
+            let sets = line.split(": ").nth(1).unwrap();
+            let sets = sets.split("; ").map(parse_set).collect::<Vec<(u32, u32, u32)>>();
+            let idx = idx + 1;
+            let is_invalid = sets.iter().filter(|set| set.0 > 12 || set.1 > 13 || set.2 > 14).count() > 0;
+            (idx, is_invalid)
+        })
+        .filter(|(_, is_invalid)| !is_invalid)
+        .map(|(idx, _)| idx)
+        .sum::<usize>()
+}
+
+fn parse_set(set: &str) -> (u32, u32, u32) {
+    let mut r = 0;
+    let mut g = 0;
+    let mut b = 0;
+
+    for x in set.split(", ") {
+        let mut parts = x.split(" ");
+        let count = parts.next().unwrap().parse::<u32>().unwrap();
+        let colour = parts.next().unwrap();
+
+        match colour {
+            "red" => r = count,
+            "green" => g = count,
+            "blue" => b = count,
+            _ => unreachable!()
+        }
+    }
+
+    (r, g, b)
+}
