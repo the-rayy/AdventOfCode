9758f375728e56a44867bef2587c40c11c57c73c
Rafał Klusek <klusek.raf@gmail.com>
2023-12-05 20:00:48 +0100

    2023 day05 part2 ohmygod

--- aoc2023/src/bin/day05.rs
+++ aoc2023/src/bin/day05.rs
@@ -1,7 +1,8 @@
+use std::cmp::max;
 use std::fs;
 use std::time::Instant;
 use std::collections::{HashMap, HashSet};
-use std::path::Component::ParentDir;
+use itertools::Itertools;
 
 fn main() {
     let input = fs::read_to_string("data/day05.txt")
@@ -12,26 +13,66 @@ fn main() {
     println!("Part 1 time: {:.2?}", part1_start.elapsed());
     println!("Part 1 ans: {:?}", part1_ans);
 
-    // let part2_start = Instant::now();
-    // let part2_ans = part2(&input);
-    // println!("Part 2 time: {:.2?}", part2_start.elapsed());
-    // println!("Part 2 ans: {:?}", part2_ans);
+    let part2_start = Instant::now();
+    let part2_ans = part2(&input);
+    println!("Part 2 time: {:.2?}", part2_start.elapsed());
+    println!("Part 2 ans: {:?}", part2_ans);
 }
+
+const MAPPING_KEYS: [&str; 7] = [
+    "seed-to-soil",
+    "soil-to-fertilizer",
+    "fertilizer-to-water",
+    "water-to-light",
+    "light-to-temperature",
+    "temperature-to-humidity",
+    "humidity-to-location"
+];
 
 fn part1(input: &str) -> u32 {
     let (seeds, mappings) = parse(input);
 
     seeds.iter()
         .map(|&s| {
-            let s = mappings.get("seed-to-soil").unwrap().map(s);
-            let s = mappings.get("soil-to-fertilizer").unwrap().map(s);
-            let s = mappings.get("fertilizer-to-water").unwrap().map(s);
-            let s = mappings.get("water-to-light").unwrap().map(s);
-            let s = mappings.get("light-to-temperature").unwrap().map(s);
-            let s = mappings.get("temperature-to-humidity").unwrap().map(s);
-            let s = mappings.get("humidity-to-location").unwrap().map(s);
+            let mut s = s.clone();
+            for key in MAPPING_KEYS {
+                s = mappings.get(key).unwrap().map(s);
+            }
             s
         }).min().unwrap()
+}
+
+fn part2(input: &str) -> u32 {
+    let (seeds, mappings) = parse(input);
+
+    let mut seeds = seeds.iter()
+        .tuples()
+        .map(|(&start, &len)| {
+            HashSet::from([start, start + len - 1])
+        })
+        .collect::<Vec<HashSet<u32>>>();
+
+    println!("{:?}", seeds);
+
+    for key in MAPPING_KEYS {
+        seeds = seeds.iter()
+            .map(|seed| {
+                let m = mappings.get(key).unwrap();
+                let min = *seed.iter().min().unwrap();
+                let max = *seed.iter().max().unwrap();
+                println!("key {} disc: {:?}", key, m.discontinuities());
+                m.discontinuities().union(seed).into_iter()
+                    .filter(|&&x| x >= min && x <= max)
+                    .map(|&x| m.map(x))
+                    // .map(|&x| x)
+                    .collect::<HashSet<u32>>()
+            })
+            .collect::<Vec<HashSet<u32>>>();
+
+        println!("after {}: {:?}", key, seeds);
+    }
+
+    *seeds.iter().flatten().min().unwrap()
 }
 
 fn parse(input: &str) -> (Vec<u32>, HashMap<&str, Mapping>){
@@ -84,6 +125,19 @@ impl Mapping {
 
         x
     }
+
+    fn discontinuities(&self) -> HashSet<u32> {
+        self.rows.iter()
+            .map(|r| {
+                [max(r.start() as i32 - 1, 0) as u32,
+                    r.start(),
+                    r.end(),
+                    r.end()+1
+                ]
+            })
+            .flatten()
+            .collect()
+    }
 }
 
 #[derive(Debug)]
@@ -98,5 +152,13 @@ impl MappingRow {
         if x >= self.source && x < self.source + self.range {
             Some(x - self.source + self.destination)
         } else { None }
+    }
+
+    fn start(&self) -> u32 {
+        self.source
+    }
+
+    fn end(&self) -> u32 {
+        self.source + self.range
     }
 }
