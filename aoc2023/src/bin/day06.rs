79b3c4dd62fa44fc17d7592a8853b139a5c64a3e
Rafał Klusek <klusek.raf@gmail.com>
2023-12-06 18:23:02 +0100

    2023 day06 part2

--- aoc2023/src/bin/day06.rs
+++ aoc2023/src/bin/day06.rs
@@ -1,8 +1,5 @@
 use std::fs;
 use std::time::Instant;
-use std::collections::HashMap;
-use std::path::Component::ParentDir;
-use itertools::Itertools;
 
 fn main() {
     let input = fs::read_to_string("data/day06.txt")
@@ -13,10 +10,10 @@ fn main() {
     println!("Part 1 time: {:.2?}", part1_start.elapsed());
     println!("Part 1 ans: {:?}", part1_ans);
 
-    // let part2_start = Instant::now();
-    // let part2_ans = part2(&input);
-    // println!("Part 2 time: {:.2?}", part2_start.elapsed());
-    // println!("Part 2 ans: {:?}", part2_ans);
+    let part2_start = Instant::now();
+    let part2_ans = part2(&input);
+    println!("Part 2 time: {:.2?}", part2_start.elapsed());
+    println!("Part 2 ans: {:?}", part2_ans);
 }
 
 fn part1(input: &str) -> u64 {
@@ -26,6 +23,12 @@ fn part1(input: &str) -> u64 {
         .zip(distance)
         .map(|(&t, d)| race(t, d))
         .product()
+}
+
+fn part2(input: &str) -> u64 {
+    let (time, distance) = parse2(input);
+
+    race(time, distance)
 }
 
 fn parse(input: &str) -> (Vec<u64>, Vec<u64>) {
@@ -48,15 +51,29 @@ fn parse(input: &str) -> (Vec<u64>, Vec<u64>) {
     (time, distance)
 }
 
+fn parse2(input: &str) -> (u64, u64) {
+    let input = input.replace(" ", "");
+    let mut splitted = input.split("\n");
+    let time = splitted.next().unwrap()
+        .strip_prefix("Time:")
+        .unwrap()
+        .parse::<u64>()
+        .unwrap();
+    let distance = splitted.next().unwrap()
+        .strip_prefix("Distance:")
+        .unwrap()
+        .parse::<u64>()
+        .unwrap();
+
+    (time, distance)
+}
+
 fn race(time: u64, distance_to_beat: u64) -> u64 {
-    let mut wins = 0;
-    for charge_time in 0..time+1 {
-        let total_distance = charge_time * (time - charge_time);
+    let d = time * time - 4 * distance_to_beat;
+    let d = (d as f64).sqrt();
 
-        if total_distance > distance_to_beat {
-            wins += 1;
-        }
-    }
+    let x1 = (time as f64 - d) / 2.0;
+    let x2 = (time as f64 + d) / 2.0;
 
-    wins
+    (x2.ceil()-x1.floor()) as u64 - 1
 }
