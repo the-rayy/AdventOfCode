c1a0926b67b779dedb25557eaf9a355f2025240d
Rafał Klusek <klusek.raf@gmail.com>
2023-12-09 01:38:44 +0100

    2023 day08 optimizations

--- aoc2023/src/bin/day08.rs
+++ aoc2023/src/bin/day08.rs
@@ -2,6 +2,7 @@ use std::collections::HashMap;
 use std::fs;
 use std::time::Instant;
 use num::integer::lcm;
+use rayon::prelude::*;
 
 fn main() {
     let input = fs::read_to_string("data/day08.txt")
@@ -18,24 +19,22 @@ fn main() {
     println!("Part 2 ans: {:?}", part2_ans);
 }
 
-const START: &str = "AAA";
-const END: &str = "ZZZ";
 
 fn part1(input: &str) -> usize {
     let (instr, nodes) = parse(input);
 
-    let mut current = String::from(START);
+    let mut current = "AAA";
     let mut step = 0;
 
     loop {
         let i = instr.get(step % instr.len()).unwrap();
         match i {
-            'L' => current = nodes.get(current.as_str()).unwrap().0.clone(),
-            'R' => current = nodes.get(current.as_str()).unwrap().1.clone(),
+            'L' => current = nodes.get(current).unwrap().0,
+            'R' => current = nodes.get(current).unwrap().1,
             _ => unreachable!()
         }
         step += 1;
-        if current.as_str() == END {
+        if current == "ZZZ" {
             break step;
         }
     }
@@ -44,52 +43,38 @@ fn part1(input: &str) -> usize {
 fn part2(input: &str) -> usize {
     let (instr, nodes) = parse(input);
 
-    let mut current = nodes.keys()
-        .filter(|k| k.ends_with("A"))
-        .map(|k| k.clone())
-        .collect::<Vec<String>>();
-    let mut lcm_compounds = current.iter()
-        .map(|_| 0)
-        .collect::<Vec<usize>>();
-    let mut step = 0;
+    nodes.keys()
+        .collect::<Vec<&&str>>()
+        .par_iter()
+        .filter(|k| { k.ends_with("A") })
+        .map(|&&k| {
+            let mut current = k;
+            let mut step = 0;
 
-    loop {
-        let i = instr.get(step % instr.len()).unwrap();
-
-        current = current.iter().map(|c| {
-            match i {
-                'L' => nodes.get(c.as_str()).unwrap().0.clone(),
-                'R' => nodes.get(c.as_str()).unwrap().1.clone(),
-                _ => unreachable!()
+            loop {
+                let i = instr.get(step % instr.len()).unwrap();
+                match i {
+                    'L' => current = &nodes.get(&current).unwrap().0,
+                    'R' => current = &nodes.get(&current).unwrap().1,
+                    _ => unreachable!()
+                }
+                step += 1;
+                if current.ends_with("Z") {
+                    break step;
+                }
             }
         })
-            .collect();
-
-        step += 1;
-
-        current.iter()
-            .enumerate()
-            .filter(|(_, val)| val.ends_with("Z"))
-            .for_each(|(idx, _)| *lcm_compounds.get_mut(idx).unwrap() = step);
-
-        if lcm_compounds.iter().all(|c| *c > 0) {
-            break
-        }
-    }
-
-    lcm_compounds.iter()
-        .fold(1, |acc, &x| lcm(acc, x) )
+        .reduce(|| 1_usize, |acc, x| lcm(acc, x) )
 }
 
 
-fn parse(input: &str) -> (Vec<char>, HashMap<String, (String, String)>) {
+fn parse(input: &str) -> (Vec<char>, HashMap<&str, (&str, &str)>) {
     let mut splitted = input.split("\n\n");
     let instr = splitted.next().unwrap().chars().collect();
     let g = splitted.next().unwrap().split("\n").map(|line| {
-        let chars = line.chars();
-        let key = chars.clone().take(3).collect::<String>();
-        let left = chars.clone().skip(7).take(3).collect::<String>();
-        let right = chars.clone().skip(12).take(3).collect::<String>();
+        let key = &line[0..3];
+        let left = &line[7..10];
+        let right = &line[12..15];
         (key, (left, right))
     })
         .collect();
