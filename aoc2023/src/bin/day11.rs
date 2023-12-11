9e92d0c4dd6f39203b67ffad71b4079f84af81c8
Rafał Klusek <klusek.raf@gmail.com>
2023-12-11 12:44:11 +0100

    2023 day11 part2

--- aoc2023/src/bin/day11.rs
+++ aoc2023/src/bin/day11.rs
@@ -14,10 +14,10 @@ fn main() {
     println!("Part 1 time: {:.2?}", part1_start.elapsed());
     println!("Part 1 ans: {:?}", part1_ans);
 
-    // let part2_start = Instant::now();
-    // let part2_ans = part2(&input);
-    // println!("Part 2 time: {:.2?}", part2_start.elapsed());
-    // println!("Part 2 ans: {:?}", part2_ans);
+    let part2_start = Instant::now();
+    let part2_ans = part2(&input);
+    println!("Part 2 time: {:.2?}", part2_start.elapsed());
+    println!("Part 2 ans: {:?}", part2_ans);
 }
 
 
@@ -80,6 +80,72 @@ fn part1(input: &str) -> usize {
             dist += empty_rows.iter()
                 .filter(|&&j| j > min_j && j < max_j)
                 .count();
+
+            dist
+        })
+        .sum::<usize>()
+}
+
+
+fn part2(input: &str) -> usize {
+    let galaxies = input.split("\n")
+        .enumerate()
+        .map(|(i, line)| {
+            line.chars()
+                .enumerate()
+                .map(|(j, c)| {
+                    ((i, j), c)
+                })
+                .collect::<Vec<_>>()
+        })
+        .flatten()
+        .filter(|(_, c)| *c == '#')
+        .map(|(pos, _)| pos)
+        .collect::<HashSet<(usize, usize)>>();
+
+    let max_i = galaxies.iter()
+        .map(|pos| pos.0)
+        .max()
+        .unwrap();
+
+    let max_j = galaxies.iter()
+        .map(|pos| pos.0)
+        .max()
+        .unwrap();
+
+    let empty_columns = (0..max_i).filter(|i| {
+        let g = galaxies.iter()
+            .filter(|pos| pos.0 == *i)
+            .count();
+        g == 0
+    })
+        .collect::<Vec<usize>>();
+
+    let empty_rows = (0..max_j).filter(|j| {
+        let g = galaxies.iter()
+            .filter(|pos| pos.1 == *j)
+            .count();
+        g == 0
+    })
+        .collect::<Vec<usize>>();
+
+    galaxies.iter()
+        .combinations(2)
+        .map(|pair| {
+            let mut dist = manhattan(pair[0], pair[1]);
+
+            let min_i = min(pair[0].0, pair[1].0);
+            let max_i = max(pair[0].0, pair[1].0);
+            let min_j = min(pair[0].1, pair[1].1);
+            let max_j = max(pair[0].1, pair[1].1);
+
+            dist += empty_columns.iter()
+                .filter(|&&i| i > min_i && i < max_i)
+                .count() * (1000000-1);
+
+            dist += empty_rows.iter()
+                .filter(|&&j| j > min_j && j < max_j)
+                .count() * (1000000-1);
 
             dist
         })
