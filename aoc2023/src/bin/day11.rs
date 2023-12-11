57d26c0bbc991382c856971ce865f8a688ad88f4
Rafał Klusek <klusek.raf@gmail.com>
2023-12-11 13:09:36 +0100

    2023 day11 refactored

--- aoc2023/src/bin/day11.rs
+++ aoc2023/src/bin/day11.rs
@@ -22,73 +22,60 @@ fn main() {
 
 
 fn part1(input: &str) -> usize {
-    let galaxies = input.split("\n")
-        .enumerate()
-        .map(|(i, line)| {
-            line.chars()
-                .enumerate()
-                .map(|(j, c)| {
-                    ((i, j), c)
-                })
-                .collect::<Vec<_>>()
-        })
-        .flatten()
-        .filter(|(_, c)| *c == '#')
-        .map(|(pos, _)| pos)
-        .collect::<HashSet<(usize, usize)>>();
+    let galaxies = parse(input);
 
-    let max_i = galaxies.iter()
-        .map(|pos| pos.0)
-        .max()
-        .unwrap();
+    let (max_i, max_j) = size_of_the_universe(&galaxies);
 
-    let max_j = galaxies.iter()
-        .map(|pos| pos.0)
-        .max()
-        .unwrap();
-
-    let empty_columns = (0..max_i).filter(|i| {
-        let g = galaxies.iter()
-            .filter(|pos| pos.0 == *i)
-            .count();
-        g == 0
-    })
-        .collect::<Vec<usize>>();
-
-    let empty_rows = (0..max_j).filter(|j| {
-        let g = galaxies.iter()
-            .filter(|pos| pos.1 == *j)
-            .count();
-        g == 0
-    })
-        .collect::<Vec<usize>>();
+    let (empty_columns, empty_rows) = find_emptiness_in_the_universe(&galaxies, max_i, max_j);
 
     galaxies.iter()
         .combinations(2)
         .map(|pair| {
-            let mut dist = manhattan(pair[0], pair[1]);
-
             let min_i = min(pair[0].0, pair[1].0);
             let max_i = max(pair[0].0, pair[1].0);
             let min_j = min(pair[0].1, pair[1].1);
             let max_j = max(pair[0].1, pair[1].1);
 
-            dist += empty_columns.iter()
+            manhattan(pair[0], pair[1]) +
+            empty_columns.iter()
                 .filter(|&&i| i > min_i && i < max_i)
-                .count();
-
-            dist += empty_rows.iter()
+                .count() +
+            empty_rows.iter()
                 .filter(|&&j| j > min_j && j < max_j)
-                .count();
-
-            dist
+                .count()
         })
         .sum::<usize>()
 }
 
 
 fn part2(input: &str) -> usize {
-    let galaxies = input.split("\n")
+    let galaxies = parse(input);
+
+    let (max_i, max_j) = size_of_the_universe(&galaxies);
+
+    let (empty_columns, empty_rows) = find_emptiness_in_the_universe(&galaxies, max_i, max_j);
+
+    galaxies.iter()
+        .combinations(2)
+        .map(|pair| {
+            let min_i = min(pair[0].0, pair[1].0);
+            let max_i = max(pair[0].0, pair[1].0);
+            let min_j = min(pair[0].1, pair[1].1);
+            let max_j = max(pair[0].1, pair[1].1);
+
+            manhattan(pair[0], pair[1]) +
+            empty_columns.iter()
+                .filter(|&&i| i > min_i && i < max_i)
+                .count() * (1000000-1) +
+            empty_rows.iter()
+                .filter(|&&j| j > min_j && j < max_j)
+                .count() * (1000000-1)
+        })
+        .sum::<usize>()
+}
+
+fn parse(input: &str) -> HashSet<(usize, usize)> {
+    input.split("\n")
         .enumerate()
         .map(|(i, line)| {
             line.chars()
@@ -101,8 +88,10 @@ fn part2(input: &str) -> usize {
         .flatten()
         .filter(|(_, c)| *c == '#')
         .map(|(pos, _)| pos)
-        .collect::<HashSet<(usize, usize)>>();
+        .collect::<HashSet<(usize, usize)>>()
+}
 
+fn size_of_the_universe(galaxies: &HashSet<(usize, usize)>) -> (usize, usize) {
     let max_i = galaxies.iter()
         .map(|pos| pos.0)
         .max()
@@ -112,7 +101,10 @@ fn part2(input: &str) -> usize {
         .map(|pos| pos.0)
         .max()
         .unwrap();
+    (max_i, max_j)
+}
 
+fn find_emptiness_in_the_universe(galaxies: &HashSet<(usize, usize)>, max_i: usize, max_j: usize) -> (Vec<usize>, Vec<usize>) {
     let empty_columns = (0..max_i).filter(|i| {
         let g = galaxies.iter()
             .filter(|pos| pos.0 == *i)
@@ -128,28 +120,7 @@ fn part2(input: &str) -> usize {
         g == 0
     })
         .collect::<Vec<usize>>();
-
-    galaxies.iter()
-        .combinations(2)
-        .map(|pair| {
-            let mut dist = manhattan(pair[0], pair[1]);
-
-            let min_i = min(pair[0].0, pair[1].0);
-            let max_i = max(pair[0].0, pair[1].0);
-            let min_j = min(pair[0].1, pair[1].1);
-            let max_j = max(pair[0].1, pair[1].1);
-
-            dist += empty_columns.iter()
-                .filter(|&&i| i > min_i && i < max_i)
-                .count() * (1000000-1);
-
-            dist += empty_rows.iter()
-                .filter(|&&j| j > min_j && j < max_j)
-                .count() * (1000000-1);
-
-            dist
-        })
-        .sum::<usize>()
+    (empty_columns, empty_rows)
 }
 
 fn manhattan(p1: &(usize, usize), p2: &(usize, usize)) -> usize {
