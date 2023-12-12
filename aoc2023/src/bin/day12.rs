db9d94d123e08068fe29661b743f954670df520d
Rafał Klusek <klusek.raf@gmail.com>
2023-12-12 11:47:47 +0100

    2023 day12 part1, but slow

---
+++ aoc2023/src/bin/day12.rs
@@ -0,0 +1,65 @@
+use std::cmp::{max, min};
+use std::collections::HashSet;
+use std::fs;
+use std::time::Instant;
+use itertools::Itertools;
+use num::abs;
+
+fn main() {
+    let input = fs::read_to_string("data/day12.txt")
+        .expect("Unable to load input file");
+
+    let part1_start = Instant::now();
+    let part1_ans = part1(&input);
+    println!("Part 1 time: {:.2?}", part1_start.elapsed());
+    println!("Part 1 ans: {:?}", part1_ans);
+
+    // let part2_start = Instant::now();
+    // let part2_ans = part2(&input);
+    // println!("Part 2 time: {:.2?}", part2_start.elapsed());
+    // println!("Part 2 ans: {:?}", part2_ans);
+}
+
+
+fn part1(input: &str) -> usize {
+    parse(input).iter()
+        .map(|line| {
+            count_possibilities(line.0, &line.1)
+        })
+        .sum()
+}
+
+fn parse(input: &str) -> Vec<(&str, Vec<u8>)> {
+    input.split("\n")
+        .map(|line| {
+            let mut splitted = line.split(" ");
+            let row = splitted.next().unwrap();
+            let nums = splitted.next().unwrap().split(",")
+                .map(|n| n.parse::<u8>().unwrap())
+                .collect();
+            (row, nums)
+        })
+        .collect()
+}
+
+fn count_possibilities(row: &str, nums: &Vec<u8>) -> usize {
+    if row.contains("?") {
+        let row1 = row.replacen("?", ".", 1);
+        let row2 = row.replacen("?", "#", 1);
+        return count_possibilities(row1.as_str(), nums) + count_possibilities(row2.as_str(), nums);
+    }
+
+    match valid(row, nums) {
+        true => 1,
+        false => 0
+    }
+}
+
+fn valid(row: &str, nums: &Vec<u8>) -> bool {
+    let actual_nums = row.split(".")
+        .filter(|part| part.len() > 0)
+        .map(|part| part.len() as u8)
+        .collect::<Vec<u8>>();
+
+    *nums == actual_nums
+}
