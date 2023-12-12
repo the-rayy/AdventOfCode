71ac4dcf7612b694ba04d43f09c798db5010478e
Rafał Klusek <klusek.raf@gmail.com>
2023-12-12 12:27:20 +0100

    2023 day12 optimizations

--- aoc2023/src/bin/day12.rs
+++ aoc2023/src/bin/day12.rs
@@ -46,20 +46,45 @@ fn count_possibilities(row: &str, nums: &Vec<u8>) -> usize {
     if row.contains("?") {
         let row1 = row.replacen("?", ".", 1);
         let row2 = row.replacen("?", "#", 1);
-        return count_possibilities(row1.as_str(), nums) + count_possibilities(row2.as_str(), nums);
+
+        let mut total = 0;
+        if partial_valid(row1.as_str(), nums) {
+            total += count_possibilities(row1.as_str(), nums);
+        }
+        if partial_valid(row2.as_str(), nums) {
+            total += count_possibilities(row2.as_str(), nums);
+        }
+        return total
     }
 
-    match valid(row, nums) {
+    match partial_valid(row, nums) {
         true => 1,
         false => 0
     }
 }
 
-fn valid(row: &str, nums: &Vec<u8>) -> bool {
-    let actual_nums = row.split(".")
+fn partial_valid(row: &str, nums: &Vec<u8>) -> bool {
+    if row.chars()
+        .filter(|c| *c != '.')
+        .count() < nums.iter().map(|x| *x as usize).sum::<usize>() {
+        return false
+    }
+
+    let row2 = row.splitn(2, "?").next().unwrap();
+    let mut actual_nums = row2.split(".")
         .filter(|part| part.len() > 0)
         .map(|part| part.len() as u8)
         .collect::<Vec<u8>>();
+    if row.len() != row2.len() && row2.ends_with("#") {
+        actual_nums.pop();
+    }
 
-    *nums == actual_nums
+    if actual_nums.len() > nums.len() {
+        return false
+    }
+
+    let nums = &nums[0..actual_nums.len()];
+
+    let ret = *nums == actual_nums;
+    return ret
 }
