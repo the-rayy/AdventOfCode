ba942369777f5ed3ffb68b77b3f46666ee067f00
Rafał Klusek <klusek.raf@gmail.com>
2023-12-13 11:35:56 +0100

    2023 day13 part1 optimization

--- aoc2023/src/bin/day13.rs
+++ aoc2023/src/bin/day13.rs
@@ -11,10 +11,10 @@ fn main() {
     println!("Part 1 time: {:.2?}", part1_start.elapsed());
     println!("Part 1 ans: {:?}", part1_ans);
 
-    // let part2_start = Instant::now();
-    // let part2_ans = part2(&input);
-    // println!("Part 2 time: {:.2?}", part2_start.elapsed());
-    // println!("Part 2 ans: {:?}", part2_ans);
+    let part2_start = Instant::now();
+    let part2_ans = part2(&input);
+    println!("Part 2 time: {:.2?}", part2_start.elapsed());
+    println!("Part 2 ans: {:?}", part2_ans);
 }
 
 
@@ -28,31 +28,30 @@ fn part1(input: &str) -> usize {
         .sum()
 }
 
+
+fn part2(input: &str) -> usize {
+    input.split("\n\n")
+        .map(|block| {
+            block.split("\n")
+                .map(|line| line.chars().collect::<Vec<char>>()).collect::<Block>()
+        })
+        .map(|block| score(&block))
+        .sum()
+}
+
 type Block = Vec<Vec<char>>;
 
 fn score(block: &Block) -> usize {
-    let possible_rows = block.iter()
-        .enumerate()
-        .tuple_windows()
-        .filter(|(c1, c2)| {
-            c1.1 == c2.1
-        })
-        .map(|(c1, _)| c1.0 + 1)
-        .collect::<Vec<usize>>();
-
-    let row = possible_rows.iter().filter(|r| is_reflected_at(block, **r)).next().unwrap_or(&0);
+    let row = match (0..block.len()-1).filter(|r| is_reflected_at(block, r+1)).next() {
+        Some(x) => x+1,
+        None => 0
+    };
 
     let transposed_block = transpose(block);
-    let possible_columns = transposed_block.iter()
-        .enumerate()
-        .tuple_windows()
-        .filter(|(c1, c2)| {
-            c1.1 == c2.1
-        })
-        .map(|(c1, _)| c1.0 + 1)
-        .collect::<Vec<usize>>();
-
-    let column = possible_columns.iter().filter(|r| is_reflected_at(&transposed_block, **r)).next().unwrap_or(&0);
+    let column = match (0..transposed_block.len()-1).filter(|r| is_reflected_at(&transposed_block, r+1)).next() {
+        Some(x) => x+1,
+        None => 0
+    };
 
     column + 100 * row
 }
