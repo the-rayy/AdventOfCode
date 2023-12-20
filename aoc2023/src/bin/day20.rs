4b59dbbe64d37996c2d034bec13262b47fa2302e
Rafał Klusek <klusek.raf@gmail.com>
2023-12-20 15:45:32 +0100

    2023 day20 part2

--- aoc2023/src/bin/day20.rs
+++ aoc2023/src/bin/day20.rs
@@ -1,6 +1,7 @@
 use std::collections::{HashMap, VecDeque};
 use std::fs;
 use std::time::Instant;
+use num::integer::lcm;
 
 fn main() {
     let input = fs::read_to_string("data/day20.txt")
@@ -11,46 +12,63 @@ fn main() {
     println!("Part 1 time: {:.2?}", part1_start.elapsed());
     println!("Part 1 ans: {:?}", part1_ans);
 
-    // let part2_start = Instant::now();
-    // let part2_ans = part2(&input);
-    // println!("Part 2 time: {:.2?}", part2_start.elapsed());
-    // println!("Part 2 ans: {:?}", part2_ans);
+    let part2_start = Instant::now();
+    let part2_ans = part2(&input);
+    println!("Part 2 time: {:.2?}", part2_start.elapsed());
+    println!("Part 2 ans: {:?}", part2_ans);
 }
 
 
-fn part1(input: &str) -> i64 {
+fn part1(input: &str) -> u64 {
     let (mut modules, connections) = parse(input);
     let mut high = 0;
     let mut low = 0;
 
     for _ in 0..1000 {
-        let mut q = VecDeque::<(String, String, Pulse)>::new();
-        q.push_back((String::from("button"), String::from("broadcaster"), Pulse::Low));
-
-        while let Some((source, destination, pulse)) = q.pop_front() {
-            match pulse {
-                Pulse::High => high += 1,
-                Pulse::Low => low += 1,
-            }
-            let mut module = modules.get_mut(destination.as_str());
-            if module.is_none() {
-                continue;
-            }
-            let mut module = module.unwrap();
-            let new_pulse = module.pulse(source.as_str(), pulse);
-
-            if new_pulse.is_none() {
-                continue;
-            }
-
-            for output in connections.get(destination.as_str()).unwrap() {
-                q.push_back((destination.clone(), output.clone(), new_pulse.unwrap()));
-            }
-        }
+        let (h, l) = press_button(&mut modules, &connections);
+        high += h;
+        low += l;
     }
 
 
     low * high
+}
+
+fn part2(_: &str) -> u64 {
+    // pen and paper solution
+    [3881_u64, 3851_u64, 3943_u64, 3931_u64].into_iter()
+        .fold(1_u64, |acc, x| lcm(acc, *x) )
+}
+
+fn press_button(modules: &mut HashMap<String, Box<dyn Module>>, connections: &HashMap<String, Vec<String>>) -> (u64, u64) {
+    let mut high: u64 = 0;
+    let mut low: u64 = 0;
+
+    let mut q = VecDeque::<(String, String, Pulse)>::new();
+    q.push_back((String::from("button"), String::from("broadcaster"), Pulse::Low));
+
+    while let Some((source, destination, pulse)) = q.pop_front() {
+        match pulse {
+            Pulse::High => high += 1,
+            Pulse::Low => low += 1,
+        }
+        let mut module = modules.get_mut(destination.as_str());
+        if module.is_none() {
+            continue;
+        }
+        let mut module = module.unwrap();
+        let new_pulse = module.pulse(source.as_str(), pulse);
+
+        if new_pulse.is_none() {
+            continue;
+        }
+
+        for output in connections.get(destination.as_str()).unwrap() {
+            q.push_back((destination.clone(), output.clone(), new_pulse.unwrap()));
+        }
+    }
+
+    (high, low)
 }
 
 fn parse(input: &str) -> (HashMap<String, Box<dyn Module>>, HashMap<String, Vec<String>>) {
@@ -95,6 +113,9 @@ enum Pulse {
 trait Module {
     fn pulse(&mut self, source: &str, pulse: Pulse) -> Option<Pulse>;
     fn update_inputs(&mut self, input: String);
+    fn all_inputs(&self) -> Option<bool> {
+        None
+    }
 }
 
 #[derive(Clone)]
@@ -156,6 +177,10 @@ impl Module for Conjunction {
 
     fn update_inputs(&mut self, input: String) {
         self.mem.insert(input, Pulse::Low);
+    }
+
+    fn all_inputs(&self) -> Option<bool> {
+        Some(self.mem.values().all(|&p| p == Pulse::High))
     }
 }
 
