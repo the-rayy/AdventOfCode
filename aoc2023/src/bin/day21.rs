644273a5e9961beb5048ecd77ab64c41d845de1f
Rafał Klusek <klusek.raf@gmail.com>
2023-12-21 21:10:02 +0100

    2023 day21 part2

--- aoc2023/src/bin/day21.rs
+++ aoc2023/src/bin/day21.rs
@@ -13,10 +13,10 @@ fn main() {
     println!("Part 1 time: {:.2?}", part1_start.elapsed());
     println!("Part 1 ans: {:?}", part1_ans);
 
-    // let part2_start = Instant::now();
-    // let part2_ans = part2(&input);
-    // println!("Part 2 time: {:.2?}", part2_start.elapsed());
-    // println!("Part 2 ans: {:?}", part2_ans);
+    let part2_start = Instant::now();
+    let part2_ans = part2(&input);
+    println!("Part 2 time: {:.2?}", part2_start.elapsed());
+    println!("Part 2 ans: {:?}", part2_ans);
 }
 
 
@@ -55,4 +55,64 @@ fn part1(input: &str) -> usize {
     }
 
     current.len()
+}
+
+
+// very good explanation: https://github.com/villuna/aoc23/wiki/A-Geometric-solution-to-advent-of-code-2023,-day-21
+fn part2(input: &str) -> usize {
+    let mut grid = input.split("\n")
+        .enumerate()
+        .map(|(i, line)| {
+            line.chars()
+                .enumerate()
+                .map(|(j, c)| ((i as i64, j as i64), c))
+                .collect::<Vec<_>>()
+        })
+        .flatten()
+        .collect::<HashMap<(i64, i64), char>>();
+
+    let start = *grid.iter().filter(|(_, &c)| c == 'S').next().unwrap().0;
+    grid.insert(start, '.');
+
+    let dims = (131, 131);
+
+    let mut visited = HashMap::<(i64, i64), usize>::new();
+    let mut queue = VecDeque::<((i64, i64), usize)>::new();
+    queue.push_back((start, 0));
+
+    while let Some((point, dist)) = queue.pop_front() {
+        if visited.contains_key(&point) {
+            continue;
+        }
+
+        visited.insert(point, dist);
+
+        for dir in vec![(0, 1), (0, -1), (1, 0), (-1, 0)] {
+            let next = (point.0 + dir.0, point.1 + dir.1);
+            if next.0 < 0 || next.0 >= dims.0 || next.1 < 0 || next.1 >= dims.1 {
+                continue;
+            }
+
+            if !visited.contains_key(&next) && grid.get(&next).unwrap() == &'.' {
+                queue.push_back((next, dist + 1));
+            }
+        }
+    }
+
+    let event_corners = visited.values()
+        .filter(|v| **v % 2 == 0 && **v > 65)
+        .count();
+    let odd_corners = visited.values()
+        .filter(|v| **v % 2 == 1 && **v > 65)
+        .count();
+
+    let maps_count = ((26501365 - (dims.0 / 2)) / dims.0) as usize;
+
+    let even = maps_count.pow(2);
+    let odd = (maps_count+1).pow(2);
+
+    odd * visited.values().filter(|v| **v % 2 == 1).count()
+    + even * visited.values().filter(|v| **v % 2 == 0).count()
+    - ((maps_count + 1) * odd_corners)
+    + (maps_count) * event_corners
 }
