53182638271e75808a55efb42680fd5add499f5a
Rafał Klusek <klusek.raf@gmail.com>
2024-12-01 10:06:16 +0100

    2024 day01 part1

---
+++ aoc2024/src/bin/day01.rs
@@ -0,0 +1,41 @@
+use std::fs;
+use std::time::Instant;
+
+fn main() {
+    let input = fs::read_to_string("data/day01.txt")
+        .expect("Unable to load input file");
+
+    let part1_start = Instant::now();
+    let part1_ans = part1(&input);
+    println!("Part 1 time: {:.2?}", part1_start.elapsed());
+    println!("Part 1 ans: {:?}", part1_ans);
+
+//    let part2_start = Instant::now();
+//    let part2_ans = part2(&input);
+//    println!("Part 2 time: {:.2?}", part2_start.elapsed());
+//    println!("Part 2 ans: {:?}", part2_ans);
+}
+
+fn part1(input: &str) -> u32 {
+    let mut left = Vec::new();
+    let mut right = Vec::new();
+
+    input.split("\n")
+        .for_each(|line| {
+            if line.is_empty() {
+                return;
+            }
+
+            let mut split = line.split(" ");
+            left.push(split.next().unwrap().parse::<i32>().unwrap());
+            right.push(split.last().unwrap().parse::<i32>().unwrap());
+        });
+
+    left.sort();
+    right.sort();
+
+    left.iter().zip(right.iter())
+        .map(|(l, r)| (l-r).abs())
+        .sum::<i32>() as u32
+}
+
