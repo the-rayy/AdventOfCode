bf0125c21bc65e7084fee847182cefebc89daa19
Rafał Klusek <klusek.raf@gmail.com>
2024-12-02 16:58:31 +0100

    2024 day01 even faster, but not so pretty

--- aoc2024/src/bin/day01.rs
+++ aoc2024/src/bin/day01.rs
@@ -40,37 +40,27 @@ fn part1(input: &str) -> u32 {
         .sum::<i32>() as u32
 }
 
-fn part2(input: &str) -> u32 {
-    let mut left = Vec::with_capacity(1000);
-    let mut right = Vec::with_capacity(1000);
-
-    input.split("\n").for_each(|line| {
-        if line.is_empty() {
-            return;
-        }
-
-        let mut split = line.split(" ");
-        left.push(split.next().unwrap().parse::<i32>().unwrap());
-        right.push(split.last().unwrap().parse::<i32>().unwrap());
-    });
-
-//    left.iter()
-//        .map(|l| right.iter().filter(|r| l == *r).count() as i32 * l)
-//        .sum::<i32>() as u32
-//    let mut right_counts: HashMap<i32, i32> = HashMap::new(); 
-//    for r in &right { 
-//        *right_counts.entry(*r).or_insert(0) += 1; 
-//    }
-    //
-    right.sort();
-
-    let mut right_counts = HashMap::with_capacity(1000);
-    for (key, chunk) in &right.iter().chunk_by(|b| *b) {
-        right_counts.insert(key, chunk.count() as i32);
-    }
  
-    left.iter() 
-        .map(|l| right_counts.get(&l).unwrap_or(&0) * l) 
-        .sum::<i32>() as u32 
-
+fn part2(input: &str) -> u32 { 
+    let mut right_counts: HashMap<i32, i32> = HashMap::with_capacity(1000); 
+    let mut sum: i32 = 0; 
+ 
+    for line in input.lines() { 
+        if line.is_empty() { 
+            continue; 
+        } 
+ 
+        let mut parts = line.split_whitespace(); 
+        let left: i32 = parts.next().unwrap().parse().unwrap(); 
+        let right: i32 = parts.next().unwrap().parse().unwrap(); 
+ 
+        *right_counts.entry(right).or_insert(0) += 1; 
+ 
+        if let Some(count) = right_counts.get(&left) { 
+            sum += count * left; 
+        }; 
+    } 
+ 
+    sum as u32 
 }
+
