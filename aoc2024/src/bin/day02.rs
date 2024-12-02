edbdebb87065df9cbd384e1cb25e054e142d9fc6
Rafał Klusek <klusek.raf@gmail.com>
2024-12-02 11:07:19 +0100

    2024 day02 part2

--- aoc2024/src/bin/day02.rs
+++ aoc2024/src/bin/day02.rs
@@ -11,26 +11,68 @@ fn main() {
     println!("Part 1 time: {:.2?}", part1_start.elapsed());
     println!("Part 1 ans: {:?}", part1_ans);
 
-//    let part2_start = Instant::now();
-//    let part2_ans = part2(&input);
-//    println!("Part 2 time: {:.2?}", part2_start.elapsed());
-//    println!("Part 2 ans: {:?}", part2_ans);
+    let part2_start = Instant::now();
+    let part2_ans = part2(&input);
+    println!("Part 2 time: {:.2?}", part2_start.elapsed());
+    println!("Part 2 ans: {:?}", part2_ans);
 }
 
 fn part1(input: &str) -> u32 {
-    input.split("\n").filter(|line| {
-        if line.is_empty() {
-            return false;
-        }
+    input
+        .split("\n")
+        .filter(|line| {
+            if line.is_empty() {
+                return false;
+            }
 
-        let diffs = line.split(" ").map(|x| x.parse::<i32>().unwrap()).tuple_windows()
-            .map(|(x, y)| x - y).collect::<Vec<i32>>();
-        
-        let same_sign = diffs.iter().all(|&x| x > 0) || diffs.iter().all(|&x| x < 0);
-        let contains_zero = diffs.iter().any(|&x| x == 0);
-        let contains_abs_gt_3 = diffs.iter().any(|&x| x.abs() > 3);
-
-        same_sign && !contains_zero && !contains_abs_gt_3
-    }).count() as u32
+            let line = line
+                .split(" ")
+                .map(|x| x.parse::<i32>().unwrap())
+                .collect::<Vec<i32>>();
+            is_safe(&line)
+        })
+        .count() as u32
 }
 
+fn part2(input: &str) -> u32 {
+    input
+        .split("\n")
+        .filter(|line| {
+            if line.is_empty() {
+                return false;
+            }
+
+            let line = line
+                .split(" ")
+                .map(|x| x.parse::<i32>().unwrap())
+                .collect::<Vec<i32>>();
+            if is_safe(&line) {
+                return true;
+            }
+
+            for idx_to_remove in 0..line.len() {
+                let mut new_line = line.clone();
+                new_line.remove(idx_to_remove);
+                if is_safe(&new_line) {
+                    return true;
+                }
+            }
+
+            false
+        })
+        .count() as u32
+}
+
+fn is_safe(input: &Vec<i32>) -> bool {
+    let diffs = input
+        .iter()
+        .tuple_windows()
+        .map(|(x, y)| x - y)
+        .collect::<Vec<i32>>();
+
+    let same_sign = diffs.iter().all(|&x| x > 0) || diffs.iter().all(|&x| x < 0);
+    let contains_zero = diffs.iter().any(|&x| x == 0);
+    let contains_abs_gt_3 = diffs.iter().any(|&x| x.abs() > 3);
+
+    same_sign && !contains_zero && !contains_abs_gt_3
+}
