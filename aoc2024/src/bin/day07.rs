5633a8ec188de7bfa62c1afd06f742c2183042cd
Rafał Klusek <klusek.raf@gmail.com>
2024-12-07 14:21:36 +0100

    2024 day07 part2

--- aoc2024/src/bin/day07.rs
+++ aoc2024/src/bin/day07.rs
@@ -11,17 +11,18 @@ fn main() {
     let part1_ans = part1(&input);
     println!("Part 1 time: {:.2?}", part1_start.elapsed());
     println!("Part 1 ans: {:?}", part1_ans);
-//
-//    let part2_start = Instant::now();
-//    let part2_ans = part2(&input);
-//    println!("Part 2 time: {:.2?}", part2_start.elapsed());
-//    println!("Part 2 ans: {:?}", part2_ans);
+
+    let part2_start = Instant::now();
+    let part2_ans = part2(&input);
+    println!("Part 2 time: {:.2?}", part2_start.elapsed());
+    println!("Part 2 ans: {:?}", part2_ans);
 }
 
 #[derive(Debug, PartialEq, Eq)]
 enum Operator {
     Add,
     Multiply,
+    Concatenate,
 }
 
 fn part1(input: &str) -> u64 {
@@ -44,6 +45,40 @@ fn part1(input: &str) -> u64 {
                 match operation[i] {
                     Operator::Add => acc += numbers[i],
                     Operator::Multiply => acc *= numbers[i],
+                    Operator::Concatenate => acc = format!("{}{}", acc, numbers[i]).parse::<u64>().unwrap(),
+                }
+            }
+
+            if acc == desired {
+                return Some(acc);
+            }
+        };
+        None
+
+    }).sum::<u64>()
+}
+
+fn part2(input: &str) -> u64 {
+    input.lines().filter_map(|line| {
+        let numbers = line.replace(":", "").split_whitespace().map(|x| x.parse::<u64>().unwrap()).collect::<Vec<u64>>();
+       
+        let desired = numbers[0];
+        let numbers = &numbers[1..];
+
+        let available_operations = vec![Operator::Add, Operator::Multiply, Operator::Concatenate];
+        let operations = itertools::repeat_n(available_operations.iter(), numbers.len()).multi_cartesian_product();
+
+        for operation in operations {
+            if *operation[0] != Operator::Add {
+                continue;
+            }
+            
+            let mut acc = 0;
+            for i in 0..numbers.len() {
+                match operation[i] {
+                    Operator::Add => acc += numbers[i],
+                    Operator::Multiply => acc *= numbers[i],
+                    Operator::Concatenate => acc = format!("{}{}", acc, numbers[i]).parse::<u64>().unwrap(),
                 }
             }
 
