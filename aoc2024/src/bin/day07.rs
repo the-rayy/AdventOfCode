867d7d0917ee52f333d3e1a76757bbdaa2684caf
Rafał Klusek <klusek.raf@gmail.com>
2024-12-07 14:41:23 +0100

    2024 day07 optimizations

--- aoc2024/src/bin/day07.rs
+++ aoc2024/src/bin/day07.rs
@@ -1,6 +1,6 @@
+use itertools::Itertools;
 use std::fs;
 use std::time::Instant;
-use itertools::Itertools;
 
 use hashbrown::HashMap;
 
@@ -26,71 +26,80 @@ enum Operator {
 }
 
 fn part1(input: &str) -> u64 {
-    input.lines().filter_map(|line| {
-        let numbers = line.replace(":", "").split_whitespace().map(|x| x.parse::<u64>().unwrap()).collect::<Vec<u64>>();
-       
-        let desired = numbers[0];
-        let numbers = &numbers[1..];
+    let available_operations = vec![Operator::Add, Operator::Multiply];
+    input
+        .lines()
+        .filter_map(|line| {
+            let numbers = line
+                .replace(":", "")
+                .split_whitespace()
+                .map(|x| x.parse::<u64>().unwrap())
+                .collect::<Vec<u64>>();
 
-        let available_operations = vec![Operator::Add, Operator::Multiply];
-        eval(desired, numbers, &available_operations)
-    }).sum::<u64>()
+            let desired = numbers[0];
+            let numbers = &numbers[1..];
+
+            eval(desired, numbers, &available_operations)
+        })
+        .sum::<u64>()
 }
 
 fn part2(input: &str) -> u64 {
-    input.lines().filter_map(|line| {
-        let numbers = line.replace(":", "").split_whitespace().map(|x| x.parse::<u64>().unwrap()).collect::<Vec<u64>>();
-       
-        let desired = numbers[0];
-        let numbers = &numbers[1..];
+    let available_operations = vec![Operator::Add, Operator::Multiply, Operator::Concatenate];
+    input
+        .lines()
+        .filter_map(|line| {
+            let numbers = line
+                .replace(":", "")
+                .split_whitespace()
+                .map(|x| x.parse::<u64>().unwrap())
+                .collect::<Vec<u64>>();
 
-        let available_operations = vec![Operator::Add, Operator::Multiply, Operator::Concatenate];
-        let operations = itertools::repeat_n(available_operations.iter(), numbers.len()).multi_cartesian_product();
-
-        for operation in operations {
-            if *operation[0] != Operator::Add {
-                continue;
-            }
-            
-            let mut acc = 0;
-            for i in 0..numbers.len() {
-                match operation[i] {
-                    Operator::Add => acc += numbers[i],
-                    Operator::Multiply => acc *= numbers[i],
-                    Operator::Concatenate => acc = format!("{}{}", acc, numbers[i]).parse::<u64>().unwrap(),
-                }
-            }
-
-            if acc == desired {
-                return Some(acc);
-            }
-        };
-        None
-
-    }).sum::<u64>()
+            let desired = numbers[0];
+            let numbers = &numbers[1..];
+            eval(desired, numbers, &available_operations)
+        })
+        .sum::<u64>()
 }
 
 fn eval(target: u64, numbers: &[u64], available_operations: &[Operator]) -> Option<u64> {
-    let operations = itertools::repeat_n(available_operations.iter(), numbers.len()).multi_cartesian_product();
+    let operations =
+        itertools::repeat_n(available_operations.iter(), numbers.len()).multi_cartesian_product();
 
-        for operation in operations {
-            if *operation[0] == Operator::Multiply {
-                continue;
-            }
-            
-            let mut acc = 0;
-            for i in 0..numbers.len() {
-                match operation[i] {
-                    Operator::Add => acc += numbers[i],
-                    Operator::Multiply => acc *= numbers[i],
-                    Operator::Concatenate => acc = format!("{}{}", acc, numbers[i]).parse::<u64>().unwrap(),
+    'outer: for operation in operations {
+        if *operation[0] != Operator::Add {
+            continue;
+        }
+
+        let mut acc = 0;
+        for i in 0..numbers.len() {
+            match operation[i] {
+                Operator::Add => acc += numbers[i],
+                Operator::Multiply => acc *= numbers[i],
+                Operator::Concatenate => {
+                    acc = concatenate_numbers(acc, numbers[i]);
                 }
             }
-
-            if acc == target {
-                return Some(acc);
+            if acc > target {
+                continue 'outer;
             }
-        };
-        None
+        }
 
+        if acc == target {
+            return Some(acc);
+        }
+    }
+    None
+}
+
+fn concatenate_numbers(a: u64, b: u64) -> u64 {
+    let mut temp_b = b;
+    let mut multiplier = 1;
+
+    while temp_b > 0 {
+        multiplier *= 10;
+        temp_b /= 10;
+    }
+
+    a * multiplier + b
 }
