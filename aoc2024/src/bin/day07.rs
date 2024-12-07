63581af5be3240f88a1615e141ec162b4601b8ab
Rafał Klusek <klusek.raf@gmail.com>
2024-12-07 14:17:40 +0100

    2024 day07 part1

---
+++ aoc2024/src/bin/day07.rs
@@ -0,0 +1,58 @@
+use std::fs;
+use std::time::Instant;
+use itertools::Itertools;
+
+use hashbrown::HashMap;
+
+fn main() {
+    let input = fs::read_to_string("data/day07.txt").expect("Unable to load input file");
+
+    let part1_start = Instant::now();
+    let part1_ans = part1(&input);
+    println!("Part 1 time: {:.2?}", part1_start.elapsed());
+    println!("Part 1 ans: {:?}", part1_ans);
+//
+//    let part2_start = Instant::now();
+//    let part2_ans = part2(&input);
+//    println!("Part 2 time: {:.2?}", part2_start.elapsed());
+//    println!("Part 2 ans: {:?}", part2_ans);
+}
+
+#[derive(Debug, PartialEq, Eq)]
+enum Operator {
+    Add,
+    Multiply,
+}
+
+fn part1(input: &str) -> u64 {
+    input.lines().filter_map(|line| {
+        let numbers = line.replace(":", "").split_whitespace().map(|x| x.parse::<u64>().unwrap()).collect::<Vec<u64>>();
+       
+        let desired = numbers[0];
+        let numbers = &numbers[1..];
+
+        let available_operations = vec![Operator::Add, Operator::Multiply];
+        let operations = itertools::repeat_n(available_operations.iter(), numbers.len()).multi_cartesian_product();
+
+        for operation in operations {
+            if *operation[0] == Operator::Multiply {
+                continue;
+            }
+            
+            let mut acc = 0;
+            for i in 0..numbers.len() {
+                match operation[i] {
+                    Operator::Add => acc += numbers[i],
+                    Operator::Multiply => acc *= numbers[i],
+                }
+            }
+
+            if acc == desired {
+                return Some(acc);
+            }
+        };
+        None
+
+    }).sum::<u64>()
+}
+
