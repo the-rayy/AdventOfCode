1a9186ab81e471b64c8f7a21819d0334029a2f39
Rafał Klusek <klusek.raf@gmail.com>
2024-12-13 11:09:55 +0100

    2024 day13 part2

--- aoc2024/src/bin/day13.rs
+++ aoc2024/src/bin/day13.rs
@@ -8,11 +8,11 @@ fn main() {
     let part1_ans = part1(&input);
     println!("Part 1 time: {:.2?}", part1_start.elapsed());
     println!("Part 1 ans: {:?}", part1_ans);
-    //
-    //let part2_start = Instant::now();
-    //let part2_ans = part2(&input);
-    //println!("Part 2 time: {:.2?}", part2_start.elapsed());
-    //println!("Part 2 ans: {:?}", part2_ans);
+
+    let part2_start = Instant::now();
+    let part2_ans = part2(&input);
+    println!("Part 2 time: {:.2?}", part2_start.elapsed());
+    println!("Part 2 ans: {:?}", part2_ans);
 }
 
 fn part1(input: &str) -> i64 {
@@ -28,6 +28,32 @@ fn part1(input: &str) -> i64 {
             let by: i64 = cap[4].parse().unwrap();
             let px: i64 = cap[5].parse().unwrap();
             let py: i64 = cap[6].parse().unwrap();
+            
+            let b = (py*ax - px*ay) / (by*ax - bx*ay);
+            let a = (px - b*bx) / ax;
+
+            if a*ax + b*bx == px && a*ay + b*by == py {
+                Some(3*a + b)
+            } else {
+                None
+            }
+        })
+        .sum()
+}
+
+fn part2(input: &str) -> i64 {
+    let pattern = r"Button A: X\+(\d+), Y\+(\d+)\nButton B: X\+(\d+), Y\+(\d+)\nPrize: X=(\d+), Y=(\d+)";
+    let pattern = regex::Regex::new(pattern).unwrap();
+
+    pattern
+        .captures_iter(input)
+        .filter_map(|cap| {
+            let ax: i64 = cap[1].parse().unwrap();
+            let ay: i64 = cap[2].parse().unwrap();
+            let bx: i64 = cap[3].parse().unwrap();
+            let by: i64 = cap[4].parse().unwrap();
+            let px: i64 = cap[5].parse::<i64>().unwrap() + 10000000000000;
+            let py: i64 = cap[6].parse::<i64>().unwrap() + 10000000000000;
             
             let b = (py*ax - px*ay) / (by*ax - bx*ay);
             let a = (px - b*bx) / ax;
