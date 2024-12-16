652910bf7aa817db22b81b89f7d8bd9cfa5698cc
Rafał Klusek <klusek.raf@gmail.com>
2024-12-16 12:50:00 +0100

    2024 day16 part2

--- aoc2024/src/bin/day16.rs
+++ aoc2024/src/bin/day16.rs
@@ -12,7 +12,7 @@ fn main() {
     println!("Part 1 ans: {:?}", part1_ans);
 
     let part2_start = Instant::now();
-    let part2_ans = part2(&input);
+    let part2_ans = part2(&input, part1_ans);
     println!("Part 2 time: {:.2?}", part2_start.elapsed());
     println!("Part 2 ans: {:?}", part2_ans);
 }
@@ -70,7 +70,7 @@ fn part1(input: &str) -> u32 {
     visited.iter().filter(|((pos, _), _)| *pos == end).map(|(_, score)| *score).min().unwrap()
 }
 
-fn part2(input: &str) -> u32 {
+fn part2(input: &str, shortest_path_score: u32) -> u32 {
     let grid: HashMap<(i32, i32), char> = input
         .lines()
         .enumerate()
@@ -90,36 +90,49 @@ fn part2(input: &str) -> u32 {
 
     let dirs = [(0, 1), (1, 0), (0, -1), (-1, 0)];
 
-    queue.push_back((start, 0, 0, 0));
-    visited.insert((start, 0), (0, 0));
+    queue.push_back((start, 0, 0, vec![start]));
+    visited.insert((start, 0), 0);
 
-    while let Some((pos, dir, score, len)) = queue.pop_front() {
+    let mut places = HashSet::new();
+    while let Some((pos, dir, score, path)) = queue.pop_front() {
+        if score > shortest_path_score {
+            continue;
+        }
+
+        if score == shortest_path_score && pos == end {
+            places.extend(path);
+            continue;
+        }
+
         //forward
         let new_pos = (pos.0 + dirs[dir].0, pos.1 + dirs[dir].1);
         if let Some(x) = grid.get(&new_pos) {
-            if *x != '#' && (visited.get(&(new_pos, dir)).is_none() || score + 1 < visited.get(&(new_pos, dir)).unwrap().0) {
-                visited.insert((new_pos, dir), (score + 1, len + 1));
-                queue.push_back((new_pos, dir, score + 1, len + 1));
+            if *x != '#' && !path.contains(&new_pos) && (visited.get(&(new_pos, dir)).is_none() || score + 1 <= *visited.get(&(new_pos, dir)).unwrap()) {
+                let mut path = path.clone();
+                path.push(new_pos);
+
+                visited.insert((new_pos, dir), score + 1);
+                queue.push_back((new_pos, dir, score + 1, path));
 
             }
         }
 
         //left
         let new_dir = (dir - 1).rem_euclid(4);
-        if visited.get(&(pos, new_dir)).is_none() || score + 1000 < visited.get(&(pos, new_dir)).unwrap().0 {
-            visited.insert((pos, new_dir), (score + 1000, len + 1));
-            queue.push_back((pos, new_dir, score + 1000, len + 1));
+        if visited.get(&(pos, new_dir)).is_none() || score + 1000 <= *visited.get(&(pos, new_dir, )).unwrap() {
+            visited.insert((pos, new_dir), score + 1000);
+            queue.push_back((pos, new_dir, score + 1000, path.clone()));
         }
 
         //right
         let new_dir = (dir + 1).rem_euclid(4);
-        if visited.get(&(pos, new_dir)).is_none() || score + 1000 < visited.get(&(pos, new_dir)).unwrap().0 {
-            visited.insert((pos, new_dir), (score + 1000, len + 1));
-            queue.push_back((pos, new_dir, score + 1000, len + 1));
+        if visited.get(&(pos, new_dir)).is_none() || score + 1000 <= *visited.get(&(pos, new_dir)).unwrap() {
+            visited.insert((pos, new_dir), score + 1000);
+            queue.push_back((pos, new_dir, score + 1000, path.clone()));
         }
 
     };
 
-    visited.iter().filter(|((pos, _), _)| *pos == end).map(|(_, score)| *score).min().unwrap().1
+    places.len() as u32   
 }
 
