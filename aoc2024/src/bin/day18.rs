a0ab106da16fedf23757026502910a7bfa4669ec
Rafał Klusek <klusek.raf@gmail.com>
2024-12-18 10:24:23 +0100

    2024 day18 part1

---
+++ aoc2024/src/bin/day18.rs
@@ -0,0 +1,64 @@
+use std::{collections::VecDeque, fs};
+use std::time::Instant;
+
+use hashbrown::{HashMap, HashSet};
+
+fn main() {
+    let input = fs::read_to_string("data/day18.txt").expect("Unable to load input file");
+
+    let part1_start = Instant::now();
+    let part1_ans = part1(&input);
+    println!("Part 1 time: {:.2?}", part1_start.elapsed());
+    println!("Part 1 ans: {:?}", part1_ans);
+
+    //let part2_start = Instant::now();
+    //let part2_ans = part2(&input);
+    //println!("Part 2 time: {:.2?}", part2_start.elapsed());
+    //println!("Part 2 ans: {:?}", part2_ans);
+}
+
+fn part1(input: &str) -> u32 {
+    let dims = (70, 70);
+    let start = (0, 0);
+    let end = (70, 70);
+    let num_obstacles = 1024;
+
+    let obstacles = input.lines().enumerate().filter_map(|(i, l)| {
+        if i >= num_obstacles { return None; }
+        let mut s = l.split(",");
+        Some((s.next().unwrap().parse::<u32>().unwrap(), s.next().unwrap().parse::<u32>().unwrap()))
+    }).collect::<HashSet<_>>();
+
+    let mut visited = HashSet::new();
+    let mut queue = VecDeque::new();
+    queue.push_back((start, 0));
+
+    while let Some((pos, steps)) = queue.pop_front() {
+        if pos == end {
+            return steps;
+        }
+
+        if visited.contains(&pos) {
+            continue;
+        }
+
+        visited.insert(pos);
+        let dirs = [(0, 1), (0, -1), (1, 0), (-1, 0)];
+        for dir in dirs.iter() {
+            let new_pos = (pos.0 as i32 + dir.0, pos.1 as i32 + dir.1);
+            if new_pos.0 < 0 || new_pos.1 < 0 || new_pos.0 > dims.0 as i32 || new_pos.1 > dims.1 as i32 {
+                continue;
+            }
+
+            let new_pos = (new_pos.0 as u32, new_pos.1 as u32);
+            if obstacles.contains(&new_pos) {
+                continue;
+            }
+
+            queue.push_back((new_pos, steps + 1));
+        }
+    }
+
+    unreachable!();
+}
+
