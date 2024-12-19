1e947255dc9ff2e49be60ba02b3f4437e03adbe4
Rafał Klusek <klusek.raf@gmail.com>
2024-12-19 11:31:10 +0100

    2024 day19 part1 optimization

--- aoc2024/src/bin/day19.rs
+++ aoc2024/src/bin/day19.rs
@@ -1,8 +1,6 @@
 use std::fs;
 use std::time::Instant;
 
-use hashbrown::HashMap;
-
 fn main() {
     let input = fs::read_to_string("data/day19.txt").expect("Unable to load input file");
 
@@ -21,27 +19,49 @@ fn part1(input: &str) -> u32 {
     let mut lines = input.lines();
     let towels = lines.next().unwrap().split(", ").map(|x| x.chars().collect::<Vec<_>>()).collect::<Vec<_>>();
     _ = lines.next(); //empty line
-    lines.map(|x| x.chars().collect::<Vec<_>>()).filter(|x| possible(&towels, x, Vec::new())).count() as u32
+    lines.map(|x| x.chars().collect::<Vec<_>>()).filter(|x| possible(&towels, x)).count() as u32
 }
 
-fn possible(towels: &Vec<Vec<char>>, design: &Vec<char>, test: Vec<char>) -> bool {
-    if test.len() == design.len() && test == *design {
+fn possible(towels: &Vec<Vec<char>>, design: &[char]) -> bool {
+    if design.len() == 0 {
         return true;
     }
 
-    if test.len() >= design.len() {
-        return false;
-    }
-
-    if test != design[0..test.len()] {
-        return false;
-    }
-
     for i in 0..towels.len() {
-        if possible(towels, design, test.clone().into_iter().chain(towels[i].clone()).collect()) {
-            return true;
+        let towel = &towels[i];
+        if towel.len() > design.len() {
+            continue;
+        }
+
+        if towel == &design[0..towel.len()] {
+            if possible(towels, &design[towel.len()..]) {
+                return true;
+            }
         }
     }
 
     false
+}
+
+fn part2(input: &str) -> u32 {
+    let mut lines = input.lines();
+    let towels = lines.next().unwrap().split(", ").map(|x| x.chars().collect::<Vec<_>>()).collect::<Vec<_>>();
+    _ = lines.next(); //empty line
+    lines.map(|x| x.chars().collect::<Vec<_>>()).map(|x| possibillities(&towels, &x, Vec::new())).sum::<u32>()
+}
+
+fn possibillities(towels: &Vec<Vec<char>>, design: &Vec<char>, test: Vec<char>) -> u32 {
+    if test.len() == design.len() && test == *design {
+        return 1;
+    }
+
+    if test.len() >= design.len() {
+        return 0;
+    }
+
+    if test != design[0..test.len()] {
+        return 0;
+    }
+
+    towels.iter().map(|x| possibillities(towels, design, test.clone().into_iter().chain(x.clone()).collect())).sum()
 }
