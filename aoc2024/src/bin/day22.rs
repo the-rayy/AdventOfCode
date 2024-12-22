e80c3bbd6ce0876ddc2e0ceb2b670f81f758659f
Rafał Klusek <klusek.raf@gmail.com>
2024-12-22 11:53:36 +0100

    2024 day22 tidy

--- aoc2024/src/bin/day22.rs
+++ aoc2024/src/bin/day22.rs
@@ -20,58 +20,74 @@ fn main() {
 }
 
 fn part1(input: &str) -> u64 {
-  let steps = 2000;
-  input.lines().map(|line| {
-    let mut h = line.parse().unwrap();
-    for _ in 0..steps {
-      h = nxt(h);
-    }
-    h
-  }).sum()
+    let steps = 2000;
+    input
+        .lines()
+        .map(|line| {
+            let mut h = line.parse().unwrap();
+            for _ in 0..steps {
+                h = nxt(h);
+            }
+            h
+        })
+        .sum()
 }
 
 fn part2(input: &str) -> u64 {
-  let steps = 2000;
+    let steps = 2000;
 
-  let hashmaps = input.lines().map(|line| {
-    let mut h = line.parse().unwrap();
-    let last_digits = (0..steps+1).map(|_| {
-      let last_digit = h % 10;
-      h = nxt(h);
-      last_digit as i8
-    }).collect::<Vec<_>>();
+    let hashmaps = input
+        .lines()
+        .map(|line| {
+            let mut h = line.parse().unwrap();
+            let last_digits = (0..steps + 1)
+                .map(|_| {
+                    let last_digit = h % 10;
+                    h = nxt(h);
+                    last_digit as i8
+                })
+                .collect::<Vec<_>>();
 
-    let diffs = last_digits.windows(2).map(|w| w[1] - w[0]).collect::<Vec<_>>();
+            let diffs = last_digits
+                .windows(2)
+                .map(|w| w[1] - w[0])
+                .collect::<Vec<_>>();
 
-    diffs.windows(4).enumerate().take(last_digits.len()-4).rev().map(|(idx, seq)| {
-      (seq.iter().cloned().collect::<Vec<_>>(), last_digits[idx+4] as u64)
-    }).collect::<HashMap<_, _>>()
+            diffs
+                .windows(4)
+                .enumerate()
+                .take(last_digits.len() - 4)
+                .rev()
+                .map(|(idx, seq)| {
+                    (
+                        seq.iter().cloned().collect::<Vec<_>>(),
+                        last_digits[idx + 4] as u64,
+                    )
+                })
+                .collect::<HashMap<_, _>>()
+        })
+        .fold(HashMap::new(), |mut acc, h| {
+            for (k, v) in h {
+                *acc.entry(k).or_insert(0) += v;
+            }
+            acc
+        });
 
-  }).fold(HashMap::new(), |mut acc, h| {
-      for (k, v) in h {
-        *acc.entry(k).or_insert(0) += v;
-      }
-      acc
-  });
-
-  let max_value = hashmaps.values().max().unwrap();
-  let max_key = hashmaps.iter().find(|(_, v)| *v == max_value).unwrap().0;
-
-  *max_value
+    *hashmaps.values().max().unwrap()
 }
 
 fn nxt(h: u64) -> u64 {
-  let res = h * 64;
-  let mix = h.bitxor(res);
-  let h = mix % 16777216;
+    let res = h * 64;
+    let mix = h.bitxor(res);
+    let h = mix % 16777216;
 
-  let res = h / 32;
-  let mix = h.bitxor(res);
-  let h = mix % 16777216;
+    let res = h / 32;
+    let mix = h.bitxor(res);
+    let h = mix % 16777216;
 
-  let res = h * 2048;
-  let mix = h.bitxor(res);
-  let h = mix % 16777216;
+    let res = h * 2048;
+    let mix = h.bitxor(res);
+    let h = mix % 16777216;
 
-  h
+    h
 }
