a4ff5e5232fb9456ef1bbc11e7af8cd2ffd500f3
Rafał Klusek <klusek.raf@gmail.com>
2025-12-01 15:43:33 +0100

    2025 day01 part2

--- aoc2025/src/bin/day01.rs
+++ aoc2025/src/bin/day01.rs
@@ -9,10 +9,10 @@ fn main() {
     println!("Part 1 time: {:.2?}", part1_start.elapsed());
     println!("Part 1 ans: {:?}", part1_ans);
 
-    // let part2_start = Instant::now();
-    // let part2_ans = part2(&input);
-    // println!("Part 2 time: {:.2?}", part2_start.elapsed());
-    // println!("Part 2 ans: {:?}", part2_ans);
+    let part2_start = Instant::now();
+    let part2_ans = part2(&input);
+    println!("Part 2 time: {:.2?}", part2_start.elapsed());
+    println!("Part 2 ans: {:?}", part2_ans);
 }
 
 fn part1(input: &str) -> u32 {
@@ -20,12 +20,46 @@ fn part1(input: &str) -> u32 {
     let mut password: u32 = 0;
 
     input.lines().for_each(|line| {
-      let num = line.replace("L", "").replace("R", "-")
+        let num = line
+            .replace("L", "-")
+            .replace("R", "")
             .parse::<i64>()
             .unwrap();
         current += num;
         if current % 100 == 0 {
             password += 1;
+        }
+    });
+
+    password
+}
+
+fn part2(input: &str) -> u32 {
+    let mut current: i64 = 50;
+    let mut password: u32 = 0;
+
+    input.lines().for_each(|line| {
+        let num = line
+            .replace("L", "-")
+            .replace("R", "")
+            .parse::<i64>()
+            .unwrap();
+        current += num;
+        if current == 0 || current == 100 {
+            password += 1;
+            current = 0;
+        }
+        if current < 0 {
+            password += (current / 100).abs() as u32 + 1;
+            if current - num == 0 {
+                password -= 1;
+            }
+            current = (current % 100) + 100;
+            current = current % 100;
+        }
+        if current > 100 {
+            password += (current / 100).abs() as u32;
+            current = current % 100;
         }
     });
 
