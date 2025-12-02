9d502cbf6bdee8531e5cb08e92b0815c55fc7f22
Rafał Klusek <klusek.raf@gmail.com>
2025-12-02 16:41:49 +0100

    2025 day02 part2

--- aoc2025/src/bin/day02.rs
+++ aoc2025/src/bin/day02.rs
@@ -2,6 +2,8 @@ use std::cmp::max;
 use std::fs;
 use std::time::Instant;
 
+use hashbrown::HashSet;
+
 fn main() {
     let input = fs::read_to_string("data/day02.txt").expect("Unable to load input file");
 
@@ -47,21 +49,17 @@ fn part1(input: &str) -> u64 {
 }
 
 fn part2(input: &str) -> u64 {
-    let mut c = 0;
-    let master = 11111111111111111_u64;
+    let mut c = HashSet::new();
     input.split(",").for_each(|line| {
         let mut s = line.trim().split("-");
         let start = s.next().unwrap();
-        let mid = max((start.len() as f64 / 2.0).floor() as usize, 1);
-        let end = s.next().unwrap().parse::<u64>().unwrap();
+        let end = s.next().unwrap();
+        let mid = max((end.len() as f64 / 2.0).floor() as usize, 1);
+        let end = end.parse::<u64>().unwrap();
 
         for i in 1..mid + 1 {
-            let mut seed = {
-                let (s, _) = start.split_at(i);
-                s.parse::<u64>().unwrap()
-            };
-            let digits = i as u64;
-            let seed = master % 10_u64.pow(digits);
+            let digits = i as u32;
+            let mut seed = 10_u64.pow(digits - 1);
             let start = start.parse::<u64>().unwrap();
 
             loop {
@@ -69,8 +67,7 @@ fn part2(input: &str) -> u64 {
                 loop {
                     foo = foo * 10_u64.pow(digits) + seed;
                     if foo >= start && foo <= end {
-                        c += foo;
-                        dbg!(start, foo, end);
+                        c.insert(foo);
                     }
 
                     if foo >= end {
@@ -79,12 +76,14 @@ fn part2(input: &str) -> u64 {
                 }
                 seed += 1;
                 let d = seed.checked_ilog10().unwrap() + 1;
-                if d != digits { break; };
+                if d != digits {
+                    break;
+                };
             }
         }
     });
 
-    c
+    c.iter().sum()
 }
 
 fn split_in_half(s: &str) -> (&str, &str) {
