bca380072304bef587c1014ef6ebf002b65e449d
Rafał Klusek <klusek.raf@gmail.com>
2025-12-02 16:05:00 +0100

    2025 day02 part1

---
+++ aoc2025/src/bin/day02.rs
@@ -0,0 +1,53 @@
+use std::cmp::max;
+use std::fs;
+use std::time::Instant;
+
+fn main() {
+    let input = fs::read_to_string("data/day02.txt").expect("Unable to load input file");
+
+    let part1_start = Instant::now();
+    let part1_ans = part1(&input);
+    println!("Part 1 time: {:.2?}", part1_start.elapsed());
+    println!("Part 1 ans: {:?}", part1_ans);
+
+    // let part2_start = Instant::now();
+    // let part2_ans = part2(&input);
+    // println!("Part 2 time: {:.2?}", part2_start.elapsed());
+    // println!("Part 2 ans: {:?}", part2_ans);
+}
+
+fn part1(input: &str) -> u64 {
+  let mut c = 0;
+    input.split(",").for_each(|line| {
+        let mut s = line.trim().split("-");
+        let start = s.next().unwrap();
+        let mut half = {
+            let (s, _) = split_in_half(start);
+            s.parse::<u64>().unwrap()
+        };
+        let start = start.parse::<u64>().unwrap();
+        let end = s.next().unwrap().parse::<u64>().unwrap();
+
+        loop {
+          let digits = half.checked_ilog10().unwrap() + 1;
+          let foo = half * 10_u64.pow(digits) + half;
+          
+          if foo >= start && foo <=end {
+            c += foo;
+          }
+
+          if foo >= end {
+            break;
+          }
+           half += 1;
+
+        }
+    });
+
+    c
+}
+
+fn split_in_half(s: &str) -> (&str, &str) {
+    let mid = max((s.len() as f64 / 2.0).floor() as usize, 1);
+    s.split_at(mid)
+}
