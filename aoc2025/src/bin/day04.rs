2f24048a209670a3afb7dd90ddf362bea71c272b
Rafał Klusek <klusek.raf@gmail.com>
2025-12-04 11:24:14 +0100

    2025 day04 part1

---
+++ aoc2025/src/bin/day04.rs
@@ -0,0 +1,35 @@
+use std::fs;
+use std::time::Instant;
+
+use hashbrown::HashSet;
+
+fn main() {
+    let input = fs::read_to_string("data/day04.txt").expect("Unable to load input file");
+
+    let part1_start = Instant::now();
+    let part1_ans = part1(&input);
+    println!("Part 1 time: {:.2?}", part1_start.elapsed());
+    println!("Part 1 ans: {:?}", part1_ans);
+
+    // let part2_start = Instant::now();
+    // let part2_ans = part2(&input);
+    // println!("Part 2 time: {:.2?}", part2_start.elapsed());
+    // println!("Part 2 ans: {:?}", part2_ans);
+}
+
+fn part1(input: &str) -> u32 {
+    let map = input.lines().enumerate().map(|(i, line)| {
+      line.chars().enumerate().filter(|(_, c)| *c == '@').map(|(j, _)| (i as i32, j as i32)).collect::<Vec<_>>()
+    }).flatten().collect::<HashSet<_>>();
+
+  map.iter().filter(|(i, j)| {
+    let mut neighbours = 0;
+    for x in -1_i32..=1 {
+      for y in -1_i32..=1 {
+        neighbours += if map.contains(&(i+x, j+y)) {1} else {0};
+      }
+    }
+    neighbours < 5
+}).count() as u32
+}
+
