fed36d81e12322ee8e003864d3e5a329f36e68a4
Rafał Klusek <klusek.raf@gmail.com>
2025-12-05 11:18:30 +0100

    2025 day05 part2

--- aoc2025/src/bin/day05.rs
+++ aoc2025/src/bin/day05.rs
@@ -1,6 +1,9 @@
 use std::fs;
 use std::time::Instant;
 
+use hashbrown::HashSet;
+use itertools::Itertools;
+
 fn main() {
     let input = fs::read_to_string("data/day05.txt").expect("Unable to load input file");
 
@@ -9,24 +12,64 @@ fn main() {
     println!("Part 1 time: {:.2?}", part1_start.elapsed());
     println!("Part 1 ans: {:?}", part1_ans);
 
-    // let part2_start = Instant::now();
-    // let part2_ans = part2(&input);
-    // println!("Part 2 time: {:.2?}", part2_start.elapsed());
-    // println!("Part 2 ans: {:?}", part2_ans);
+    let part2_start = Instant::now();
+    let part2_ans = part2(&input);
+    println!("Part 2 time: {:.2?}", part2_start.elapsed());
+    println!("Part 2 ans: {:?}", part2_ans);
 }
 
 fn part1(input: &str) -> u32 {
     let mut s = input.split("\n\n");
-    let ranges = s.next().unwrap().lines().map(|line| {
-      let mut s = line.split("-");
-      let start = s.next().unwrap().parse::<usize>().unwrap();
-      let end = s.next().unwrap().parse::<usize>().unwrap();
-      start..=end
-    }).collect::<Vec<_>>();
+    let ranges = s
+        .next()
+        .unwrap()
+        .lines()
+        .map(|line| {
+            let mut s = line.split("-");
+            let start = s.next().unwrap().parse::<usize>().unwrap();
+            let end = s.next().unwrap().parse::<usize>().unwrap();
+            start..=end
+        })
+        .collect::<Vec<_>>();
 
-    s.next().unwrap().lines().filter(|line| {
-      let n = line.trim().parse::<usize>().unwrap();
-      ranges.iter().any(|r| r.contains(&n))
-    }).count() as u32
+    s.next()
+        .unwrap()
+        .lines()
+        .filter(|line| {
+            let n = line.parse::<usize>().unwrap();
+            ranges.iter().any(|r| r.contains(&n))
+        })
+        .count() as u32
 }
 
+fn part2(input: &str) -> usize {
+    let ranges = input
+        .split("\n\n")
+        .next()
+        .unwrap()
+        .lines()
+        .map(|line| {
+            let mut s = line.split("-");
+            let start = s.next().unwrap().parse::<usize>().unwrap();
+            let end = s.next().unwrap().parse::<usize>().unwrap();
+            start..=end
+        })
+        .sorted_by_key(|r| *r.start())
+        .collect::<Vec<_>>();
+
+    let mut merged = Vec::new();
+    let mut current = ranges[0].clone();
+
+    for r in ranges.into_iter().skip(1) {
+        if r.start() <= current.end() {
+            current = *current.start()..=(*current.end()).max(*r.end());
+        } else {
+            merged.push(current);
+            current = r;
+        }
+    }
+
+    merged.push(current);
+
+    merged.into_iter().map(|m| m.count()).sum::<usize>()
+}
