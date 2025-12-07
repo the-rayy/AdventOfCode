85f607ad41a7c1d1aea304ad930d9aa144aaf1f5
Rafał Klusek <klusek.raf@gmail.com>
2025-12-07 14:17:00 +0100

    2025 day07 part2

--- aoc2025/src/bin/day07.rs
+++ aoc2025/src/bin/day07.rs
@@ -1,6 +1,8 @@
 use std::fs;
 use std::time::Instant;
 
+use hashbrown::HashMap;
+
 fn main() {
     let input = fs::read_to_string("data/day07.txt").expect("Unable to load input file");
 
@@ -9,10 +11,10 @@ fn main() {
     println!("Part 1 time: {:.2?}", part1_start.elapsed());
     println!("Part 1 ans: {:?}", part1_ans);
 
-    // let part2_start = Instant::now();
-    // let part2_ans = part2(&input);
-    // println!("Part 2 time: {:.2?}", part2_start.elapsed());
-    // println!("Part 2 ans: {:?}", part2_ans);
+    let part2_start = Instant::now();
+    let part2_ans = part2(&input);
+    println!("Part 2 time: {:.2?}", part2_start.elapsed());
+    println!("Part 2 ans: {:?}", part2_ans);
 }
 
 fn part1(input: &str) -> u64 {
@@ -31,11 +33,38 @@ fn part1(input: &str) -> u64 {
                 split_counter += 1;
             }
             if c == '.' && beams.contains(&i) {
-              new_beams.push(i);
+                new_beams.push(i);
             }
         }
         beams = new_beams;
     });
 
-  split_counter
+    split_counter
+}
+
+fn part2(input: &str) -> u64 {
+    let mut beams = HashMap::<usize, u64>::new();
+
+    input.lines().for_each(|line| {
+        let mut new_beams = HashMap::new();
+        for (i, c) in line.chars().enumerate() {
+            if c == 'S' {
+                new_beams.insert(i, 1);
+            };
+            if c == '^' {
+                let foo = beams.get(&i).unwrap_or(&0).clone();
+                let foo2 = new_beams.get(&(i - 1)).unwrap_or(&0).clone();
+                let foo3 = new_beams.get(&(i + 1)).unwrap_or(&0).clone();
+                new_beams.insert(i - 1, foo + foo2);
+                new_beams.insert(i + 1, foo + foo3);
+            }
+            if c == '.' && beams.contains_key(&i) {
+                let foo = beams.get(&i).unwrap().clone();
+                let foo2 = new_beams.get(&i).unwrap_or(&0).clone();
+                new_beams.insert(i, foo + foo2);
+            }
+        }
+        beams = new_beams;
+    });
+    beams.values().sum::<u64>() as u64
 }
