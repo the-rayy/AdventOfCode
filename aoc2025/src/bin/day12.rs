451f034b82e041b5a1a25ee268c89398ae2a687b
Rafał Klusek <klusek.raf@gmail.com>
2025-12-12 13:28:35 +0100

    2025 day12 part1

--- aoc2025/src/bin/day12.rs
+++ aoc2025/src/bin/day12.rs
@@ -12,15 +12,19 @@ fn main() {
 
 fn part1(input: &str) -> u64 {
   input
-    .split("\n\n")
-    .last()
-    .unwrap()
     .lines()
+    .skip(30)
     .filter(|input| {
       let dimx = &input[0..2].parse::<usize>().unwrap();
       let dimy = &input[3..5].parse::<usize>().unwrap();
-      let req_area = &input[7..].split(" ").map(|x| x.parse::<usize>().unwrap() * 9).sum::<usize>();
+      let s0 = &input[7..9].parse::<usize>().unwrap();
+      let s1 = &input[10..12].parse::<usize>().unwrap();
+      let s2 = &input[13..15].parse::<usize>().unwrap();
+      let s3 = &input[16..18].parse::<usize>().unwrap();
+      let s4 = &input[19..21].parse::<usize>().unwrap();
+      let s5 = &input[22..24].parse::<usize>().unwrap();
 
+      let req_area = (s0 + s1 + s2 + s3 + s4 + s5) * 9;
       let area = dimx * dimy;
 
       area >= req_area
